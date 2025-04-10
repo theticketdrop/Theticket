aa61186153cad03f6ecc03b52067c58c40698982
theticketdrop <theticket1@gmx.com>
2025-04-10 10:03:15 +0200

    Update create-payment-intent.js

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,21 +1,14 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async function(event, context) {
-  try {
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: 100, // 1 €
-      currency: "eur",
-      automatic_payment_methods: { enabled: true } // Active Apple Pay, CB, Google Pay
-    });
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 100,
+    currency: "eur",
+    automatic_payment_methods: { enabled: true }
+  });
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ clientSecret: paymentIntent.client_secret })
-    };
-  } catch (err) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: err.message })
-    };
-  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ clientSecret: paymentIntent.client_secret })
+  };
 };
