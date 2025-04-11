e07ed548de44d7ecc56bba9809e50119b6c895d7
theticketdrop <theticket1@gmx.com>
2025-04-11 14:22:27 +0200

    Update create-payment-intent.js

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,14 +1,28 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async (event) => {
-  const { email } = JSON.parse(event.body || '{}');
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: 100,
-    currency: "eur",
-    receipt_email: email,
+  const { email } = JSON.parse(event.body || "{}");
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card", "apple_pay"],
+    line_items: [
+      {
+        price_data: {
+          currency: "eur",
+          product_data: { name: "The Ticket" },
+          unit_amount: 100,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: "https://theticket.netlify.app/confirmation",
+    cancel_url: "https://theticket.netlify.app/checkout",
+    customer_email: email,
   });
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ clientSecret: paymentIntent.client_secret }),
+    body: JSON.stringify({ url: session.url }),
   };
 };
